49e11be1fb818bc29299aae4fdf5e7c7b353f2af
thleima <thomas61.amiel@gmail.com>
2024-12-22 08:39:01 +0100

    add comments to index.js

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+// 'O' is for available, 'X' is for unavailable
+// Set all the seats available 
 const theaterSeats = [
   ['O', 'O', 'O'],
   ['O', 'O', 'O'],
@@ -6,23 +8,11 @@ const theaterSeats = [
 
 const seatsBooked = []
 
-function updateSeatStatus(row, col, status){
-  const seats = document.getElementsByClassName('seat');
-  const index = row * 3 + col;
-  seats[index].classList.remove('available');
-  seats[index].classList.add(status);
-}
-
-function displaySelected(seatsBooked){
-  const text = document.getElementById('display')
-  if (seatsBooked.length === 1) {
-    text.innerText = `Seats Selected is : ${seatsBooked[0]}`
-  } else {
-    text.innerText = `Seats Selected are : ${seatsBooked}`
-    
-  }
-}
-
+/* Book a seat : 
+1) update the status in the 2D Array 
+2) update the class in HTML to have the color change 
+3) display all seats selected
+*/
 function bookSeat(row, col){
   if (theaterSeats[row][col] === 'O'){
     theaterSeats[row][col] = 'X';
@@ -34,17 +24,30 @@ function bookSeat(row, col){
   }
 }
 
-
-function checkAvailableSeats(availableSeats, theaterSeats){
-  for (let row=0; row < theaterSeats.length; row++){
-    for (let col = 0; col < theaterSeats[row].length; col++){
-      if(theaterSeats[row][col]==='O'){
-        availableSeats.push({row, col});
-      }
-    }
+// bookSeat() Helper function n°1
+function updateSeatStatus(row, col, status){
+  const seats = document.getElementsByClassName('seat');
+  const index = row * 3 + col;
+  seats[index].classList.remove('available');
+  seats[index].classList.add(status);
+}
+// bookSeat() Helper function n°2
+function displaySelected(seatsBooked){
+  const text = document.getElementById('display')
+  if (seatsBooked.length === 1) {
+    text.innerText = `Seats Selected is : ${seatsBooked[0]}`
+  } else {
+    text.innerText = `Seats Selected are : ${seatsBooked}`
+    
   }
 }
 
+/*
+Book a random seat :
+1) check available seats, by scanning the theatherSeats 2D array
+2) book a random seat if available
+3) update the 2D array status with bookSeat() function
+*/
 function bookRandomSeat(){
   const availableSeats = [];
   checkAvailableSeats(availableSeats, theaterSeats);
@@ -56,3 +59,15 @@ function bookRandomSeat(){
     alert('All seats are already booked')
   }
 }
+
+// bookRandomSeat() Helper function 
+function checkAvailableSeats(availableSeats, theaterSeats){
+  for (let row=0; row < theaterSeats.length; row++){
+    for (let col = 0; col < theaterSeats[row].length; col++){
+      if(theaterSeats[row][col]==='O'){
+        availableSeats.push({row, col});
+      }
+    }
+  }
+}
+
